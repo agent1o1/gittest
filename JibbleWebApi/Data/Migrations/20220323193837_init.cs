dcad40d19dfa5c7f5c07b693df7d61b4ccd2bf1d
Andrew <zhuravandrey@gmail.com>
2022-03-23 23:06:34 +0300

    dbcontext  + beginning of user auth

---
+++ JibbleWebApi/Data/Migrations/20220323193837_init.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImdbID = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Salt = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ JibbleWebApi/JibbleWebApi/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Data;
+using JibbleWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data.Services;
+using Data.Entities;
+
+namespace JibbleWebApi.Controllers
+{
+    [Route("login")]
+    public class LoginController : ControllerBase
+    {
+        private readonly MoviesContext _moviesContext;
+        private readonly IUserService _userService;
+
+        public LoginController(MoviesContext moviesContext, IUserService userService)
+        {
+            _moviesContext = moviesContext;
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if (!string.IsNullOrEmpty(model.UserName) || !string.IsNullOrEmpty(model.Password))
+                return BadRequest("User name and password must be not empty");
+
+            if (await _moviesContext.Users.AsQueryable().Where(x => x.UserName == model.UserName).AnyAsync())
+                return BadRequest("Username already exist");
+
+            var salt = _userService.GenerateSalt();
+            var user = new User { Password = _userService.GeneratePasswordHash(model.Password, salt), UserName = model.UserName };
+
+            await _moviesContext.AddAsync(user);
+
+            await _moviesContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
