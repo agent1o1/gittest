01c9976bf384b9eb8aebaf6ee0f39de9d6c37c87
Andrew <zhuravandrey@gmail.com>
2022-03-24 15:45:34 +0300

    done with auth

---
+++ JibbleWebApi/Data/Services/AuthenticationService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Data.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Data.Services
+{
+    public interface IAuthenticationService
+    {
+        string GetAccessToken(int userId);
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IOptions<AuthenticationOptions> _options;
+
+        public AuthenticationService(IOptions<AuthenticationOptions> options)
+        {
+            _options = options;
+        }
+
+        public string GetAccessToken(int userId)
+        {
+            var identity = GetIdentity(userId);
+
+            var now = DateTime.UtcNow;
+
+            var jwt = new JwtSecurityToken(
+                issuer: _options.Value.Issuer,
+                audience: _options.Value.Audience,
+                claims: identity.Claims,
+                notBefore: now,
+                expires: now.Add(TimeSpan.FromMinutes(_options.Value.Lifetime)),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_options.Value.Secret)), SecurityAlgorithms.HmacSha256));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+        private ClaimsIdentity GetIdentity(int id)
+        {
+            var claims = new List<Claim>
+            {
+                new("ID", id.ToString()),
+            };
+            ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType, null);
+            return claimsIdentity;
+        }
+    }
+}
--- JibbleWebApi/Data/Services/UserService.cs
+++ JibbleWebApi/Data/Services/UserService.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Services
 {
     public interface IUserService
     {
-        string GenerateSalt();
-        string GeneratePasswordHash(string password, string salt);
+        Task<User> CreateUser(string userName, string password);
+
+        Task<bool> CheckPassword(User user, string password);
     }
 
     public class UserService : IUserService
     {
-        public string GeneratePasswordHash(string password, string salt)
+
+        public async Task<User> CreateUser(string userName, string password)
+        {
+            var salt = GenerateSalt();
+            return new User { Password = GeneratePasswordHash(password, salt), UserName = userName, Salt = salt };
+        }
+
+        public async Task<bool> CheckPassword(User user, string password)
+        {
+            if (user == null)
+                throw new ArgumentException("User is null");
+
+            if (GeneratePasswordHash(password, user.Salt) == user.Password)
+                return true;
+            return false;
+        }
+
+        private string GeneratePasswordHash(string password, string salt)
         {
             var hashAlgorithm = SHA256.Create();
             byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes($"{password}.{salt}"));
@@ -27,13 +47,12 @@ namespace Data.Services
             return sBuilder.ToString();
         }
 
-        public string GenerateSalt()
-{
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-            byte[] buffer = new byte[12];
-
-            rng.GetBytes(buffer);
-            return BitConverter.ToString(buffer);
+        private string GenerateSalt()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, 12)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
